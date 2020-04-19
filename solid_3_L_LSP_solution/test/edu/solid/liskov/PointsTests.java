c899c888a60cd1ff13308c1f87d3146590216f04
domingosebastian <domingo.sebastian@gmail.com>
2020-04-19 16:05:29 +0200

    liskov

--- solid_3_L_LSP_solution/test/edu/solid/liskov/PointsTests.java
+++ solid_3_L_LSP_solution/test/edu/solid/liskov/PointsTests.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 
 public class PointsTests {
 
+	// this value shows the subtype Point3D can not safely replace the Point2D type
 	private final Point2D reference = new Point3D(3,5,1);
+	
 	private final List<Point2D> test_points = Arrays.asList(
 			                                         new Point2D(2,3), 
 			                                         new Point2D(3,5), 
